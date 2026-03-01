ef29d3308d962d33822d8945fb795c84d6e2156d
polina <polina17022007@mail.ru>
2026-03-01 17:47:28 +0300

    feat: add description column to the front-end table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -78,7 +78,7 @@ function App() {
   return (
     <div>
       <header className="app-header">
-        <h1>Items hello</h1>
+        <h1>Items hello! </h1>
         <button className="btn-disconnect" onClick={handleDisconnect}>
           Disconnect
         </button>
